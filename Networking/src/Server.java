7c6857244eece1849a4a70b2b19ca8defeb829a3
mo <you@example.com>
2019-12-01 20:19:09 -0500

    showing client's hostname and ip address

--- Networking/src/Server.java
+++ Networking/src/Server.java
@@ -1,6 +1,7 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
@@ -12,17 +13,20 @@ public class Server {
             ServerSocket serverSocket = new ServerSocket(8000);
             System.out.println("Server started @ " + new Date());
             Socket socket = serverSocket.accept();
+            InetAddress inetAddress = socket.getInetAddress();
 
             DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
             DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
 
-            while (true) {
-                double radius = inputFromClient.readDouble();
-                double area = radius * radius * Math.PI;
-                outputToClient.writeDouble(area);
-                System.out.println("Radius received from client: " + radius);
-                System.out.println("Area is: " + area);
-            }
+            double radius = inputFromClient.readDouble();
+            double area = radius * radius * Math.PI;
+            outputToClient.writeDouble(area);
+
+            System.out.println("Host Name: " + inetAddress.getHostName());
+            System.out.println("Client's IP address: " + inetAddress.getHostAddress());
+            System.out.println("Radius received from client: " + radius);
+            System.out.println("Area is: " + area);
+
 
         } catch (IOException ex) {
             ex.printStackTrace();
